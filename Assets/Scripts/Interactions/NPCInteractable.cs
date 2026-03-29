3dc25e7f37734963ee77f17ec313b1de05c6c9cd
SlipperySalmon999 <noelgalan999@gmail.com>
2026-03-29 15:23:23 +1100

    may as well commit this as well

--- Assets/Scripts/Interactions/NPCInteractable.cs
+++ Assets/Scripts/Interactions/NPCInteractable.cs
@@ -1,9 +1,37 @@
-using UnityEngine;
+/*using UnityEngine;
 
 public class NPCInteractable : MonoBehaviour, IInteractable {
-    [SerializeField] private string interactiveText; 
+    [SerializeField] private string interactiveText;
     public void Interact() {
         Debug.Log(interactiveText);
+    }
+
+    public string GetInteractiveText() {
+        return interactiveText;
+    }
+}
+*/
+
+
+using UnityEngine;
+using TMPro;
+public class NPCInteractable : MonoBehaviour
+{
+
+    [SerializeField] private string interactiveText;
+    [SerializeField] private GameObject speechBubble;
+    [SerializeField] private float interactThingoOffset;
+    [SerializeField] private string dialogue;
+    public void Interact() {
+        Debug.Log("Interacting with NPC");
+      
+        GameObject bubble = Instantiate(speechBubble, new Vector3(transform.position.x, transform.position.y - interactThingoOffset, transform.position.z), transform.rotation);
+      
+        TextMeshPro textComponent = bubble.GetComponentInChildren<TextMeshPro>();
+      
+        textComponent.text = dialogue;
+      
+        Debug.Log(textComponent.text);
     }
     
     public string GetInteractiveText() {
