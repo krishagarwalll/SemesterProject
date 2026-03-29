837945500a68e500b8b076a05f628674936b4e87
SlipperySalmon999 <noelgalan999@gmail.com>
2026-03-29 15:31:40 +1100

    Simple dialogue system works. very crude but useable

--- Assets/Scripts/Interactions/NPCInteractable.cs
+++ Assets/Scripts/Interactions/NPCInteractable.cs
@@ -1,11 +1,24 @@
 using UnityEngine;
+using TMPro;
 
 public class NPCInteractable : MonoBehaviour, IInteractable {
-    [SerializeField] private string interactiveText; 
+    [SerializeField] private string interactiveText;
+    [SerializeField] private GameObject speechBubble;
+    [SerializeField] private float interactThingoOffset;
+    [SerializeField] private string dialogue;
     public void Interact() {
         Debug.Log(interactiveText);
+        
+        GameObject bubble = Instantiate(speechBubble, new Vector3(transform.position.x, transform.position.y - interactThingoOffset, transform.position.z), transform.rotation);
+      
+        TextMeshPro textComponent = bubble.GetComponentInChildren<TextMeshPro>();
+      
+        textComponent.text = dialogue;
+      
+        Debug.Log(textComponent.text);
+        
     }
-    
+
     public string GetInteractiveText() {
         return interactiveText;
     }
