d800ceb590887894f7f1aba81dabf20a1b02893c
SlipperySalmon999 <noelgalan999@gmail.com>
2026-04-30 00:52:16 +1000

    initital commit gonna make a puzzle minigame

---
+++ Assets/Scripts/Minigame/SnapEdges.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SnapEdges : MonoBehaviour
+{
+    //Make it that a puzzle piece can store an array of puzzle piecesd that connect to it
+    //then wee need a way to know how they connecct
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
