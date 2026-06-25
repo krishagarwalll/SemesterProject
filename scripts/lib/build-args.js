874dd14d57fbe5af80c43552fd1628423d297984
Niclas <niclas.rogulski@gmail.com>
2026-06-25 12:25:55 +1000

    Add build-args.js for Unity build output path and CLI arg construction

---
+++ scripts/lib/build-args.js
@@ -0,0 +1,22 @@
+const path = require('node:path');
+
+function buildOutputRelPath(platformCfg, productName) {
+  if (!platformCfg.exeSuffix) {
+    return platformCfg.buildDir;
+  }
+  return path.join(platformCfg.buildDir, `${productName}${platformCfg.exeSuffix}`);
+}
+
+function buildUnityArgs({ repoRoot, buildProfileAbsPath, outputAbsPath, logFileAbsPath }) {
+  return [
+    '-batchmode',
+    '-quit',
+    '-nographics',
+    '-projectPath', repoRoot,
+    '-activeBuildProfile', buildProfileAbsPath,
+    '-build', outputAbsPath,
+    '-logFile', logFileAbsPath,
+  ];
+}
+
+module.exports = { buildOutputRelPath, buildUnityArgs };
